09a20e968616c99f99ea9b4f1419d274587bdd48
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2022-03-31 14:50:55 -0300

    Ejercicios 1 a 5

--- Assets/Ejercicio2.cs
+++ Assets/Ejercicio2.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,
+//respectivamente, realice la operación correspondiente y muestre el resultado en pantalla:
+//a.Suma
+//b.Producto
 public class Ejercicio2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        int num1 = 4;
+        int num2 = 5;
+
+        int res = num1 + num2;
+        Debug.Log(" el resultado es " + res);
+
+        int result = num1 * num2;
+        Debug.Log(" el resultado es " + result);
     }
 
     // Update is called once per frame
--- Assets/Ejercicio4.cs
+++ Assets/Ejercicio4.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Realizá un programa que permita ingresar dos números enteros num1 y num2 por
+//Inspector. Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena
+//utilizando el siguiente formato: “La suma entre “ + num1 + “ y “ + num2 + “ da como
+//resultado “ + num1 + num2.
 
 public class Ejercicio4 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        int num1 = 2;
+        int num2 = 4;
+
+        int res = num1 + num2;
+        Debug.Log(" La suma entre " + num1 + " y " + num2 + " da como resultado " + res);
     }
 
     // Update is called once per frame
